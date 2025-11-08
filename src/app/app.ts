150402ccacfc47be41c9c30887e773b07dc563ae
Luis <luisfofitochuqimia@gmail.com>
2025-11-08 19:16:22 -0400

    feat(index.html-styles.css-app.css-app.html.app.ts):Probamos el funcionamiento del proyecto - Resolver el funcioamiento del proyexto agregando codigo de html,css y js

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
 export class App {
-  protected readonly title = signal('angular-10-app');
+  protected readonly informacion = "Angular";
 }
